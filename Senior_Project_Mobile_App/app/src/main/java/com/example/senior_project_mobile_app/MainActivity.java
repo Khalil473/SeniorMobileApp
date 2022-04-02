e84f623f61f38187b12636d197b852e4e2ed71e4
aliabualrob12 <ali.rajeh2612@gmail.com>
2022-04-02 13:53:33 -0700

    demo

--- Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/MainActivity.java
+++ Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.senior_project_mobile_app;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Switch;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     public static String currentScreen="";
@@ -23,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
     {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.setCustomAnimations(R.anim.slide_in,R.anim.fade_out);
+        if(fragment instanceof Fragment_Login_Screen )
+            fragmentTransaction.setCustomAnimations(R.anim.enter_down_to_top,R.anim.fade_out,
+                    R.anim.enter_down_to_top,R.anim.fade_out );
+        if(fragment instanceof Fragment_Main_Screen)
+            fragmentTransaction.setCustomAnimations(R.anim.enter_down_to_top,R.anim.fade_out);
+        if(fragment instanceof Fragment_Blutooth_Not_Connected_Screen )
+            fragmentTransaction.setCustomAnimations(R.anim.enter_down_to_top,R.anim.fade_out);
+
         fragmentTransaction.replace(R.id.frame_layout,fragment);
         fragmentTransaction.addToBackStack("back to connect with bluetooth screen");
         fragmentTransaction.commit();
