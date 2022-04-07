820744d97bb48a79fd558416fd8fcce30318aea2
Khalil473 <khalil.3amer2000@gmail.com>
2022-04-07 22:46:58 +0300

    compined ashraf work to show nearby devices with the bluetooth device.

--- Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/Fragment_Bluetooth_Not_Connected_Screen.java
+++ Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/Fragment_Bluetooth_Not_Connected_Screen.java
@@ -12,9 +12,7 @@ import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.fragment.app.Fragment;
-
 import java.util.ArrayList;
 
 public class Fragment_Bluetooth_Not_Connected_Screen extends Fragment {
@@ -31,24 +29,18 @@ public class Fragment_Bluetooth_Not_Connected_Screen extends Fragment {
     v = inflater.inflate(R.layout.bluetooth_not_connected_black, container, false);
     TextView tv = v.findViewById(R.id.TextView_bluetooth_not_connected_black_id);
     tv.setOnClickListener(v -> myActivity.replaceFragment(new Fragment_Login_Screen(myActivity)));
-     pb = v.findViewById(R.id.loading_bar);
+    pb = v.findViewById(R.id.loading_bar);
     tv.setOnClickListener(
         new View.OnClickListener() {
           @Override
           public void onClick(View view) {
-            if (myActivity.shoe.getStatus() == Shoe.STATE_CONNECTED) {
-              myActivity.shoe.startReading();
-              if (myActivity.shoe.isFirstTime())
-                myActivity.replaceFragment(new Fragment_Login_Screen(myActivity));
-              else myActivity.replaceFragment(new Fragment_Setup_Screen(myActivity));
-              myActivity.shoe.stopBluetoothScan();
-            }
             // myActivity.replaceFragment(new Fragment_Login_Screen(myActivity));
             if (myActivity.shoe.getStatus() == Shoe.STATE_SCANNING) {
               Toast.makeText(myActivity, "Please wait until the search Finish", Toast.LENGTH_SHORT)
                   .show();
               return;
             } // TODO: disable the search while the bluetooth is searching.
+
             myActivity.shoe.initializeDevice();
             switch (myActivity.shoe.getStatus()) {
               case Shoe.STATE_BLUETOOTH_NOT_SUPPORTED:
@@ -81,45 +73,59 @@ public class Fragment_Bluetooth_Not_Connected_Screen extends Fragment {
                     new OnBluetoothSearchFinishedListener() {
                       @Override
                       public void bluetoothSearchFinishedListener(ArrayList<String> devices) {
-                        TextView textView =v.findViewById(R.id.TextView_bluetooth_not_connected_black_id);
+                        TextView textView =
+                            v.findViewById(R.id.TextView_bluetooth_not_connected_black_id);
                         textView.setText("Refresh");
-                        LinearLayout linearLayout = v.findViewById(R.id.Linearlayout_name_of_near_devices);
+                        LinearLayout linearLayout =
+                            v.findViewById(R.id.Linearlayout_name_of_near_devices);
                         linearLayout.setBackgroundColor(Color.WHITE);
 
                         ListView listView = new ListView(myActivity);
-
-                        if(devices.isEmpty())
-                        {
-                          linearLayout.removeAllViews();
+                        linearLayout.removeAllViews();
+                        if (devices.isEmpty()) {
                           TextView no_nearby_devices_message = new TextView(myActivity);
-                          no_nearby_devices_message.setText("There are no Devises please refresh and search again");
+                          no_nearby_devices_message.setText(
+                              "There are no Devises please refresh and search again");
                           no_nearby_devices_message.setTextSize(20);
                           no_nearby_devices_message.setTextColor(Color.WHITE);
                           linearLayout.setBackgroundColor(Color.BLACK);
                           linearLayout.addView(no_nearby_devices_message);
+                          pb.setVisibility(View.INVISIBLE);
                           return;
                         }
-                        ArrayAdapter arrayAdapter = new ArrayAdapter(myActivity,android.R.layout.simple_list_item_1,devices);
+                        ArrayAdapter arrayAdapter =
+                            new ArrayAdapter(
+                                myActivity, android.R.layout.simple_list_item_1, devices);
                         listView.setAdapter(arrayAdapter);
                         linearLayout.addView(listView);
-                        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                          @Override
-                          public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                            String item = listView.getAdapter().getItem(position).toString();
-                            if (item.equals("our device"))
-                            {
-                              myActivity.replaceFragment(new Fragment_Login_Screen(myActivity));
-                            }
-                            else
-                              myActivity.replaceFragment(new Fragment_Bluetooth_Not_Connected_Screen(myActivity));
-                          }
-                        });
-                        myActivity.shoe.connectToDevice(devices.get(0));
+                        listView.setOnItemClickListener(
+                            new AdapterView.OnItemClickListener() {
+                              @Override
+                              public void onItemClick(
+                                  AdapterView<?> parent, View view, int position, long id) {
+                                String item = listView.getAdapter().getItem(position).toString();
+                                myActivity.shoe.connectToDevice(item);
+                              }
+                            });
                         pb.setVisibility(View.INVISIBLE);
                       }
                     });
 
-                myActivity.shoe.setOnBluetoothConnectedListener(() -> {});
+                myActivity.shoe.setOnBluetoothConnectedListener(
+                    () -> {
+                      tv.setText("Continue to Login/Setup");
+                      tv.setOnClickListener(
+                          (v) -> {
+                            myActivity.shoe.startReading();
+                            if (myActivity.shoe.getStatus() == Shoe.STATE_CONNECTED) {
+                              myActivity.shoe.startReading();
+                              if (myActivity.shoe.isFirstTime())
+                                myActivity.replaceFragment(new Fragment_Login_Screen(myActivity));
+                              else
+                                myActivity.replaceFragment(new Fragment_Setup_Screen(myActivity));
+                            }
+                          });
+                    });
 
                 myActivity.shoe.setOnBluetoothDisconnectedListener(
                     () -> myActivity.replaceFragment(Fragment_Bluetooth_Not_Connected_Screen.this));
