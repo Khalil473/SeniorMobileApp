368a298cf7d0e2e2ee21236afa8ce9c564bab5bd
aliabualrob12 <ali.rajeh2612@gmail.com>
2022-04-07 06:41:22 -0700

    delete animation if statements from replace fragment in main activity,Prevent turning back when we are in login , setup, main screens ,and delete the transitions ,go to top,go to down  animation files .

--- Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/MainActivity.java
+++ Senior_Project_Mobile_App/app/src/main/java/com/example/senior_project_mobile_app/MainActivity.java
@@ -22,51 +22,13 @@ public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager = getSupportFragmentManager();
     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-    if (fragment instanceof Fragment_Setup_Screen
-        || fragment instanceof Fragment_Setup_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Login_Screen
-        || fragment instanceof Fragment_Login_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Bluetooth_Not_Connected_Screen
-        || fragment instanceof Fragment_Blutooth_Not_Connected_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Main_Screen || fragment instanceof Fragment_Main_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Weight_History_Screen
-        || fragment instanceof Fragment_Weight_History_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Tempreture_History_screen
-        || fragment instanceof Fragment_Tempreture_History_screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Humadity_Screen
-        || fragment instanceof Fragment_Humadity_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Speed_History
-        || fragment instanceof Fragment_Speed_History_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Carried_Weight_History
-        || fragment instanceof Fragment_Carried_Weight_History_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_Settings_Screen
-        || fragment instanceof Fragment_Settings_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-    if (fragment instanceof Fragment_BMI_Screen || fragment instanceof Fragment_BMI_Screen_Light)
-      fragmentTransaction.setCustomAnimations(
-          R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
-
+    fragmentTransaction.setCustomAnimations(
+        R.anim.anim_to_right, R.anim.fade_out, R.anim.anim_to_left, R.anim.fade_out);
     fragmentTransaction.replace(R.id.frame_layout, fragment);
-    fragmentTransaction.addToBackStack("back to connect with bluetooth screen");
+    if (!(fragment instanceof Fragment_Login_Screen
+        || fragment instanceof Fragment_Setup_Screen
+        || fragment instanceof Fragment_Main_Screen))
+      fragmentTransaction.addToBackStack("back to connect with bluetooth screen");
     fragmentTransaction.commit();
   }
 }
